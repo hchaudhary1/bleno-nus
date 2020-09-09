a8ba5057aca88a5bd60c0c12ecdffe6508ebff53
Hassan Chaudhary <hassan789@gmail.com>
2020-09-09 02:28:52 -0700

    update main server as NUS

--- emulator/main.js
+++ emulator/main.js
@@ -76,7 +76,7 @@ bleno.on('stateChange', function(state) {
   console.log('on -> stateChange: ' + state);
 
   if (state === 'poweredOn') {
-    bleno.startAdvertising('IntelOb-XXXXXXXX', ['ec00']);
+    bleno.startAdvertising('IntelOb-XXXXXXXX', ['6E400001-B5A3-F393-E0A9-E50E24DCCA9E']);
   } else {
     bleno.stopAdvertising();
   }
