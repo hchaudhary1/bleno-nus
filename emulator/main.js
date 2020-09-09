026263a6760dea46258b8d1fc07782116761a004
Hassan Chaudhary <hassan789@gmail.com>
2020-09-08 22:56:06 -0700

    1-file

--- emulator/main.js
+++ emulator/main.js
@@ -1,10 +1,104 @@
+const util = require('util');
 var bleno = require('bleno-mac');
 const BlenoPrimaryService = bleno.PrimaryService;
 
-const RxCharacteristic = require('./rx-characteristic');
-const TxCharacteristic = require('./tx-characteristic')
+const BlenoCharacteristic = bleno.Characteristic;
+
+var tx_hook = null;
+
+const RxCharacteristic = function() {
+  RxCharacteristic.super_.call(this, {
+    uuid: '6E400003-B5A3-F393-E0A9-E50E24DCCA9E',
+    properties: ['read', 'write', 'notify'],
+    value: null
+  });
+
+  this._value = new Buffer(0);
+  this._updateValueCallback = null;
+};
+
+util.inherits(RxCharacteristic, BlenoCharacteristic);
+
+RxCharacteristic.prototype.onReadRequest = function(offset, callback) {
+  console.log('RxCharacteristic - onReadRequest: value = ' + this._value.toString('hex'));
+
+  callback(this.RESULT_SUCCESS, this._value);
+};
+
+RxCharacteristic.prototype.onSubscribe = function(maxValueSize, updateValueCallback) {
+  console.log('RxCharacteristic - onSubscribe');
+
+  this._updateValueCallback = updateValueCallback;
+  //this._updateValueCallback
+
+  tx_hook = updateValueCallback;
+  tx_hook(Buffer.from('{"serial":123456}', 'utf8'));
+};
+
+RxCharacteristic.prototype.onUnsubscribe = function() {
+  console.log('RxCharacteristic - onUnsubscribe');
+
+  this._updateValueCallback = null;
+};
+
+const rx = new RxCharacteristic();
+
+
+
+
+const TxCharacteristic = function() {
+  TxCharacteristic.super_.call(this, {
+    uuid: '6E400002-B5A3-F393-E0A9-E50E24DCCA9E',
+    properties: ['read', 'write', 'notify'],
+    value: null
+  });
+
+  this._value = new Buffer(0);
+  this._updateValueCallback = null;
+};
+
+util.inherits(TxCharacteristic, BlenoCharacteristic);
+
+TxCharacteristic.prototype.onReadRequest = function(offset, callback) {
+  console.log('TxCharacteristic - onReadRequest: value = ' + this._value.toString('hex'));
+
+  callback(this.RESULT_SUCCESS, this._value);
+};
+
+TxCharacteristic.prototype.onWriteRequest = function(data, offset, withoutResponse, callback) {
+  this._value = data;
+
+  console.log('TxCharacteristic - onWriteRequest: value = ' + this._value.toString());
+
+  if (this._updateValueCallback) {
+    console.log('TxCharacteristic - onWriteRequest: notifying');
+    
+    tx_hook(Buffer.from('bleno: rx cmd', 'utf8'));
+  }
+
+  callback(this.RESULT_SUCCESS);
+};
+
+TxCharacteristic.prototype.onSubscribe = function(maxValueSize, updateValueCallback) {
+  console.log('TxCharacteristic - onSubscribe');
+
+  this._updateValueCallback = updateValueCallback;
+};
+
+TxCharacteristic.prototype.onUnsubscribe = function() {
+  console.log('TxCharacteristic - onUnsubscribe');
+
+  this._updateValueCallback = null;
+};
+
+
+
+const tx = new TxCharacteristic();
+
 
 console.log('bleno - nordic UART');
+
+
 
 bleno.on('stateChange', function(state) {
   console.log('on -> stateChange: ' + state);
@@ -16,12 +110,12 @@ bleno.on('stateChange', function(state) {
   }
 });
 
+
+
 bleno.on('advertisingStart', function(error) {
   console.log('on -> advertisingStart: ' + (error ? 'error ' + error : 'success'));
 
   if (!error) {
-    const tx = new TxCharacteristic();
-    const rx = new RxCharacteristic();
     bleno.setServices([
       new BlenoPrimaryService({
         uuid: '6E400001-B5A3-F393-E0A9-E50E24DCCA9E',
@@ -31,6 +125,14 @@ bleno.on('advertisingStart', function(error) {
   }
 });
 
+
+
 bleno.on('disconnect', function(clientAddress) {
   console.log(`disconnect:i${clientAddress}`)
 });
+
+
+
+
+
+
